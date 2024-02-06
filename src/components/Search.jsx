ac927953ef7bec4f981513a0f175fd4a8099408c
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-02-06 11:10:37 +0530

    feat: search-com

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -11,17 +11,17 @@ const Search = () => {
         </label>
         <div className="relative">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <IoIosSearch className="text-xl" />
+            <IoIosSearch className="text-2xl text-white" />
           </div>
           <input
             type="search"
             id="default-search"
-            className="block w-full p-2 ps-10 text-sm text-gray-900 border  border-gray-700 border-opacity-40 bg-[#6529dd]"
+            className="block w-full p-2 ps-10 text-sm text-gray-200 border  border-gray-200 border-opacity-40 bg-[#6529dd] rounded-lg font-bold outline-none"
             placeholder="Search"
           />
           <h3
             type="submit"
-            className="text-white absolute end-2.5 bottom-2.5 font-medium text-sm px-4 mt-2  bg-[#4b1ea5] bg-opacity-25"
+            className="absolute end-2.5 bottom-2.5 font-medium text-sm px-1 mt-3 bg-[#4b1ea5] bg-opacity-25 text-gray-400 inline-block"
           >
             Ctrl K
           </h3>
