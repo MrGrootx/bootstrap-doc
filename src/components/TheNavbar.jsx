0df0bf8cbfcba0c36cf27983dca480ed1b6c88a4
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-02-06 11:18:48 +0530

    updated: nav-bar

--- src/components/TheNavbar.jsx
+++ src/components/TheNavbar.jsx
@@ -1,4 +1,6 @@
 import { FaBootstrap } from "react-icons/fa";
+import { FaGithub, FaTwitter } from "react-icons/fa";
+
 import Search from "./Search";
 const TheNavbar = () => {
   return (
@@ -6,24 +8,36 @@ const TheNavbar = () => {
       <section className="bg-[#702df6] ">
         <div className="container mx-auto">
           <div className="flex justify-between p-2">
-
             <div className="flex items-center">
               <FaBootstrap className="text-4xl" />
               <ul className="flex ml-3 gap-x-3">
-               <li className="text-white font-bold">Docs</li>
-               <li className="text-white">Examples</li>
-               <li className="text-white">Icons</li>
-               <li className="text-white">Themes</li>
-               <li className="text-white">Blog</li>
+                <li className=" font-bold text-gray-300 hover:text-gray-300 cursor-pointer">
+                  Docs
+                </li>
+                <li className=" text-gray-300 hover:text-gray-200 cursor-pointer">
+                  Examples
+                </li>
+                <li className=" text-gray-300 hover:text-gray-200 cursor-pointer">
+                  Icons
+                </li>
+                <li className=" text-gray-300 hover:text-gray-200 cursor-pointer">
+                  Themes
+                </li>
+                <li className=" text-gray-300 hover:text-gray-200 cursor-pointer">
+                  Blog
+                </li>
               </ul>
             </div>
 
             <div>
-               <Search/>
+              <Search />
             </div>
 
-            <div>
-            test
+            <div className="flex">
+              <div className="flex items-center gap-x-2">
+                <FaGithub className="text-xl text-gray-300 hover:text-gray-100 cursor-pointer" />
+                <FaTwitter className="text-xl text-gray-300 hover:text-gray-100 cursor-pointer" />
+              </div>
             </div>
           </div>
         </div>
